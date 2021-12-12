7e1f0b5a8986bb0f5f507de351c196e4eb50acf4
João Paulo <81633820+joaopaulo-D@users.noreply.github.com>
2021-12-12 00:50:58 -0300

    update

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from PIL import Image
 import numpy as np
 from werkzeug.utils import secure_filename
 
-model_file = "model.h5"
-model = load_model(model_file)
+modelo_file = "model.h5"
+modelo = load_model(modelo_file)
 
 app = Flask(__name__,template_folder='templates')
 UPLOAD_FOLDER = 'static'
@@ -23,7 +23,7 @@ def makePredictions(path):
       rgbimg = img_d
   rgbimg = np.array(rgbimg,dtype=np.float64)
   rgbimg = rgbimg.reshape((1,224,224,3))
-  predictions = model.predict(rgbimg)
+  predictions = modelo.predict(rgbimg)
   a = int(np.argmax(predictions))
   if a == 1:
     a = "Pneumonia"
@@ -31,7 +31,7 @@ def makePredictions(path):
     a = "Normal"
   return a
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/', methods=['GET','POST'])
 def home():
     if request.method == 'POST':
         if 'img' not in request.files:
